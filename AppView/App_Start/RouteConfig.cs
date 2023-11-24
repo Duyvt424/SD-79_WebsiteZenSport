3944105419438519cd210d21fff8bfde0f57f3dc
DoanHuyen250894 <huyendtph24484@fpt.edu.vn>
2023-11-24 10:04:20 +0700

    Huyen Commit

---
+++ AppView/App_Start/RouteConfig.cs
@@ -0,0 +1,20 @@
+﻿using System.Web.Mvc;
+using Microsoft.Data.SqlClient;
+
+namespace AppView.App_Start
+{
+    public class RouteConfig
+
+    {
+        public static void RegisterRoutes(RouteCollection routes)
+        {/*
+            routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
+
+            routes.MapRoute(
+                name: "Default",
+                url: "{controller}/{action}/{id}",
+                defaults: new { controller = "SMS", action = "SMSIndex", id = UrlParameter.Optional }
+            );*/
+        }
+    }
+}
---
+++ AppView/Models/SMSViewModel.cs
@@ -0,0 +1,8 @@
+﻿namespace AppView.Models
+{
+	public class SMSViewModel
+	{
+		public string TelNo { get; set; }
+		public string Message { get; set; }
+	}
+}
