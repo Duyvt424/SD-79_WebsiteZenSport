7ebb0b2018ed197cf3ddd88e6b5d14c9d1693e16
KieuAnh <anhhkph24928@fpt.edu.vn>
2023-12-29 14:00:33 +0700

    addcontroller

---
+++ AppView/Controllers/SizesController.cs
@@ -0,0 +1,85 @@
+﻿using AppData.IRepositories;
+using AppData.Models;
+using AppData.Repositories;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Newtonsoft.Json;
+
+namespace AppView.Controllers
+{
+    public class SizesController : Controller
+    {
+        private readonly IAllRepositories<Size> _repos;
+        private ShopDBContext _dbcontext = new ShopDBContext();
+        private DbSet<Size> _size;
+
+        public SizesController()
+        {
+            _size = _dbcontext.Sizes;
+            AllRepositories<Size> all = new AllRepositories<Size>(_dbcontext, _size);
+            _repos = all;
+        }
+        private string GenerateSizeCode()
+        {
+            var lastSize = _dbcontext.Sizes.OrderByDescending(c => c.SizeCode).FirstOrDefault();
+            if (lastSize != null)
+            {
+                var lastNumber = int.Parse(lastSize.SizeCode.Substring(2)); // Lấy phần số cuối cùng từ ColorCode
+                var nextNumber = lastNumber + 1; // Tăng giá trị cuối cùng
+                var newSizeCode = "SZ" + nextNumber.ToString("D3"); // Tạo ColorCode mới
+                return newSizeCode;
+            }
+            return "SZ001"; // Trường hợp không có ColorCode trong cơ sở dữ liệu, trả về giá trị mặc định "CL001"
+        }
+        public async Task<IActionResult> GetAllSize()
+        {
+            string apiUrl = "https://localhost:7036/api/Size/get-size";
+            var httpClient = new HttpClient();
+            var response = await httpClient.GetAsync(apiUrl);
+            string apiData = await response.Content.ReadAsStringAsync();
+            var size = JsonConvert.DeserializeObject<List<Size>>(apiData);
+            return View(size);
+        }
+        [HttpGet]
+        public async Task<IActionResult> CreateSize()
+        {
+            return View();
+        }
+        [HttpPost]
+        public async Task<IActionResult> CreateSize(Size size)
+        {
+            var httpClient = new HttpClient();
+            string apiUrl = $"https://localhost:7036/api/Size/create-size?SizeCode={GenerateSizeCode()}&Name={size.Name}&Status={size.Status}&DateCreated={size.DateCreated}";
+            var response = await httpClient.PostAsync(apiUrl, null);
+            return RedirectToAction("GetAllSize");
+        }
+        [HttpGet]
+        public async Task<IActionResult> EditSize(Guid id) // Khi ấn vào Create thì hiển thị View
+        {
+            // Lấy Product từ database dựa theo id truyền vào từ route
+            Size size = _repos.GetAll().FirstOrDefault(c => c.SizeID == id);
+            return View(size);
+        }
+        public async Task<IActionResult> EditSize(Size size) // Thực hiện việc Tạo mới
+        {
+            var httpClient = new HttpClient();
+            string apiUrl = $"https://localhost:7036/api/Size/update-size?sizeID={size.SizeID}&SizeCode={size.SizeCode}&Name={size.Name}&Status={size.Status}&DateCreated={size.DateCreated}";
+            var response = await httpClient.PutAsync(apiUrl, null);
+            return RedirectToAction("GetAllSize");
+        }
+        public async Task<IActionResult> DeleteSize(Guid id)
+        {
+            var sz = _repos.GetAll().First(c => c.SizeID == id);
+            var httpClient = new HttpClient();
+            string apiUrl = $"https://localhost:7036/api/Size/delete-size?sizeID={id}";
+            var response = await httpClient.DeleteAsync(apiUrl);
+            return RedirectToAction("GetAllSize");
+
+        }
+        public async Task<IActionResult> FindSize(string searchQuery)
+        {
+            var size = _repos.GetAll().Where(c => c.SizeCode.ToLower().Contains(searchQuery.ToLower()) || c.Name.ToLower().Contains(searchQuery.ToLower()));
+            return View(size);
+        }
+    }
+}
---
+++ AppView/Controllers/StylesController.cs
@@ -0,0 +1,91 @@
+﻿using AppData.IRepositories;
+using AppData.Models;
+using AppData.Repositories;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Newtonsoft.Json;
+
+namespace AppView.Controllers
+{
+    public class StylesController : Controller
+    {
+        private readonly IAllRepositories<Style> repos;
+        private ShopDBContext context = new ShopDBContext();
+        private DbSet<Style> style;
+
+        public StylesController()
+        {
+            style = context.Styles;
+            AllRepositories<Style> all = new AllRepositories<Style>(context, style);
+            repos = all;
+        }
+        private string GenerateStyleCode()
+        {
+            var lastStyle = context.Styles.OrderByDescending(c => c.StyleCode).FirstOrDefault();
+            if (lastStyle != null)
+            {
+                var lastNumber = int.Parse(lastStyle.StyleCode.Substring(2)); // Lấy phần số cuối cùng từ ColorCode
+                var nextNumber = lastNumber + 1; // Tăng giá trị cuối cùng
+                var newStyleCode = "ST" + nextNumber.ToString("D3")  ;
+                return newStyleCode;
+            }
+            return "ST001"; // Trường hợp không có ColorCode trong cơ sở dữ liệu, trả về giá trị mặc định "CL001"
+        }
+
+        public async Task<IActionResult> GetAllStyles()
+        {
+            string apiUrl = "https://localhost:7036/api/Style/get-style"; ;
+            var httpClient = new HttpClient();
+            var response = await httpClient.GetAsync(apiUrl);
+            string apiData = await response.Content.ReadAsStringAsync();
+            var styles = JsonConvert.DeserializeObject<List<Style>>(apiData);
+            return View(styles);
+        }
+
+        public async Task<IActionResult> CreateStyle()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> CreateStyle(Style style)
+        {
+            var httpClient = new HttpClient();
+            string apiUrl = $"https://localhost:7036/api/Style/create-style?StyleCode={GenerateStyleCode()}&name={style.Name}&status={style.Status}&DateCreated={style.DateCreated.ToString("yyyy-MM-ddTHH:mm:ss")  }";
+            var response = await httpClient.PostAsync(apiUrl, null);
+            return RedirectToAction("GetAllStyles")  ;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> EditStyle(Guid id) // Khi ấn vào Create thì hiển thị View
+        {
+            // Lấy Product từ database dựa theo id truyền vào từ route
+            Style style = repos.GetAll().FirstOrDefault(c => c.StyleID == id);
+            return View(style);
+        }
+
+        public async Task<IActionResult> EditStyle(Style style) // Thực hiện việc Tạo mới
+        {
+            var httpClient = new HttpClient();
+            string apiUrl = $"https://localhost:7036/api/Style/update-style?id={style.StyleID}&StyleCode={style.StyleCode}&name={style.Name}&status={style.Status}&DateCreated={style.DateCreated.ToString("yyyy-MM-ddTHH:mm:ss")  }";
+            var response = await httpClient.PutAsync(apiUrl, null);
+            return RedirectToAction("GetAllStyles")  ;
+        }
+
+        public async Task<IActionResult> DeleteStyle(Guid id)
+        {
+            var style = repos.GetAll().First(c => c.StyleID == id);
+            var httpClient = new HttpClient();
+            string apiUrl = $"https://localhost:7036/api/Style/delete-style?id={id}";
+            var response = await httpClient.DeleteAsync(apiUrl);
+            return RedirectToAction("GetAllStyles")  ;
+        }
+        public async Task<IActionResult> FindStyle(string searchQuery)
+        {
+            var style = repos.GetAll().Where(c => c.StyleCode.ToLower().Contains(searchQuery.ToLower()) || c.Name.ToLower().Contains(searchQuery.ToLower()));
+            return View(style);
+        }
+      
+
+    }
+}
