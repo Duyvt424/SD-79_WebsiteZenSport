7aa3eb438507c6e063499a73ff29af5a69f7b16c
KieuAnh <anhhkph24928@fpt.edu.vn>
2023-12-29 17:29:14 +0700

    kieuanhcm

---
+++ AppView/Controllers/AddresssController.cs
@@ -0,0 +1,190 @@
+﻿using AppData.IRepositories;
+using AppData.Models;
+using AppData.Repositories;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using Newtonsoft.Json;
+using Address = AppData.Models.Address;
+
+namespace AppView.Controllers
+{
+    public class AddresssController : Controller
+    {
+        private readonly IAllRepositories<Address> _repos;
+        private ShopDBContext _dbContext = new ShopDBContext();
+        private DbSet<Address> _address;
+        public AddresssController()
+        {
+            _address = _dbContext.Addresses;
+            AllRepositories<Address> all = new AllRepositories<Address>(_dbContext, _address);
+            _repos = all;
+        }
+        public async Task<IActionResult> GetAllAddress()
+        {
+            string apiUrl = "https://localhost:7036/api/Address/get-address";
+            var httpClient = new HttpClient(); // tạo ra để callApi
+            var response = await httpClient.GetAsync(apiUrl);// Lấy dữ liệu ra
+            string apiData = await response.Content.ReadAsStringAsync();
+            var address = JsonConvert.DeserializeObject<List<Address>>(apiData);
+            return View(address);
+        }
+
+        [HttpPost]
+        public IActionResult SetDefaultAddress(Guid addressId, Guid idCus)
+        {
+            // Kiểm tra xem địa chỉ có tồn tại không
+            var address = _dbContext.Addresses.FirstOrDefault(c => c.AddressID == addressId);
+            if (address == null)
+            {
+                return NotFound();
+            }
+            var userIdString = HttpContext.Session.GetString("UserId");
+            var customerIdSession = !string.IsNullOrEmpty(userIdString) ? JsonConvert.DeserializeObject<Guid>(userIdString) : Guid.Empty;
+            var customerId = customerIdSession != Guid.Empty ? customerIdSession : idCus;
+            // Lấy địa chỉ mặc định hiện tại của người dùng
+            var currentDefaultAddress = _dbContext.Addresses.FirstOrDefault(c => c.CumstomerID == customerId && c.IsDefaultAddress);
+            if (currentDefaultAddress != null)
+            {
+                // Hủy đặt làm mặc định cho địa chỉ hiện tại
+                currentDefaultAddress.IsDefaultAddress = false;
+            }
+            // Đặt làm mặc định cho địa chỉ mới
+            address.IsDefaultAddress = true;
+            // Lưu thay đổi vào cơ sở dữ liệu
+            _dbContext.SaveChanges();
+            return Ok();
+        }
+
+
+        [HttpPost]
+        public IActionResult SetDefaultAddressModal(Guid addressId, Guid idCus, Guid idBill)
+        {
+            var objBill = _dbContext.Bills.First(c => c.BillID == idBill && c.CustomerID == idCus);
+            // Kiểm tra xem địa chỉ có tồn tại không
+            var address = _dbContext.Addresses.FirstOrDefault(c => c.AddressID == addressId);
+            if (address == null)
+            {
+                return NotFound();
+            }
+            var userIdString = HttpContext.Session.GetString("UserId");
+            var customerIdSession = !string.IsNullOrEmpty(userIdString) ? JsonConvert.DeserializeObject<Guid>(userIdString) : Guid.Empty;
+            var customerId = customerIdSession != Guid.Empty ? customerIdSession : idCus;
+            // Lấy địa chỉ mặc định hiện tại của người dùng
+            var currentDefaultAddress = _dbContext.Addresses.FirstOrDefault(c => c.CumstomerID == customerId && c.IsDefaultAddress);
+            if (currentDefaultAddress != null)
+            {
+                // Hủy đặt làm mặc định cho địa chỉ hiện tại
+                currentDefaultAddress.IsDefaultAddress = false;
+            }
+            // Đặt làm mặc định cho địa chỉ mới
+            address.IsDefaultAddress = true;
+            objBill.AddressID = address.AddressID;
+            _dbContext.Bills.Update(objBill);
+            // Lưu thay đổi vào cơ sở dữ liệu
+            _dbContext.SaveChanges();
+            return Ok();
+        }
+
+        [HttpGet]
+        public IActionResult GetDefaultAddress(Guid idCus)
+        {
+            var userIdString = HttpContext.Session.GetString("UserId");
+            var customerIdSession = !string.IsNullOrEmpty(userIdString) ? JsonConvert.DeserializeObject<Guid>(userIdString) : Guid.Empty;
+            var customerId = customerIdSession != Guid.Empty ? customerIdSession : idCus;
+            // Lấy địa chỉ mặc định của người dùng
+            var defaultAddress = _dbContext.Addresses.FirstOrDefault(c => c.CumstomerID == customerId && c.IsDefaultAddress == true);
+            if (defaultAddress == null)
+            {
+                // Không tìm thấy địa chỉ mặc định
+                return NotFound();
+            }
+            // Trả về thông tin địa chỉ mặc định
+            return Ok(new { addressId = defaultAddress.AddressID });
+        }
+
+        [HttpPost]
+        public IActionResult UpdatePriceShipping(Guid addressId, decimal shippingCost)
+        {
+            var objAddress = _dbContext.Addresses.First(c => c.AddressID == addressId);
+            if (objAddress == null)
+            {
+                return NotFound();
+            }
+            objAddress.ShippingCost = shippingCost;
+            _dbContext.Update(objAddress);
+            _dbContext.SaveChanges();
+            return Ok(new { success = true });
+        }
+
+        [HttpPost]
+        public IActionResult UpdatePriceShippingInBill(Guid billId, decimal shippingCost, decimal priceVoucher)
+        {
+            var cartItems = _dbContext.BillDetails.Where(c => c.BillID == billId).Include(x => x.ShoesDetails_Size).ToList();
+            // Tính tổng giá tiền cho sản phẩm trong giỏ hàng
+            decimal totalProductPrice = cartItems.Sum(item =>
+            {
+                var shoesDetails = _dbContext.ShoesDetails.FirstOrDefault(c => c.ShoesDetailsId == item.ShoesDetails_Size.ShoesDetailsId);
+                return shoesDetails.Price * item.Quantity;
+            });
+            var objBill = _dbContext.Bills.First(c => c.BillID == billId);
+            if (objBill == null)
+            {
+                return NotFound();
+            }
+            objBill.ShippingCosts = shippingCost;
+            objBill.TotalPrice = totalProductPrice + shippingCost;
+            objBill.TotalPriceAfterDiscount = priceVoucher != null ? (totalProductPrice + shippingCost) - priceVoucher : (totalProductPrice + shippingCost) - 0;
+            _dbContext.Update(objBill);
+            _dbContext.SaveChanges();
+            return Ok(new { success = true });
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> CreateAddress()
+        {
+            using (ShopDBContext shopDBContext = new ShopDBContext())
+            {
+                var customer = shopDBContext.Customers.ToList();
+                SelectList selectListCustomer = new SelectList(customer, "CumstomerID", "UserName");
+                ViewBag.CustomerList = selectListCustomer;
+            }
+            return View();
+        }
+        [HttpPost]
+        public async Task<IActionResult> CreateAddress(Address address)
+        {
+            HttpClient httpClient = new HttpClient();
+            string apiUrl = $"https://localhost:7036/api/Address/create-address?Street={address.Street}&Commune={address.Commune}&District={address.District}&Province={address.Province}&IsDefaultAddress={address.IsDefaultAddress}&ShippingCost={address.ShippingCost}&Status={address.Status}&DateCreated={DateTime.Now.ToString("yyyy-MM-ddTHH:mm:ss")}&CumstomerID={address.CumstomerID}";
+            var response = await httpClient.PostAsync(apiUrl, null);
+            return RedirectToAction("GetAllAddress");
+        }
+        [HttpGet]
+        public async Task<IActionResult> UpdateAddress(Guid id)
+        {
+            Address address = _repos.GetAll().FirstOrDefault(c => c.AddressID == id);
+            using (ShopDBContext shopDBContext = new ShopDBContext())
+            {
+                var customer = shopDBContext.Customers.ToList();
+                SelectList selectListCustomer = new SelectList(customer, "CumstomerID", "UserName");
+                ViewBag.CustomerList = selectListCustomer;
+            }
+            return View(address);
+        }
+        public async Task<IActionResult> UpdateAddress(Address address)
+        {
+            HttpClient httpClient = new HttpClient();
+            string apiUrl = $"https://localhost:7036/api/Address/update-address?AddressID={address.AddressID}&Street={address.Street}&Commune={address.Commune}&District={address.District}&Province={address.Province}&IsDefaultAddress={address.IsDefaultAddress}&ShippingCost={address.ShippingCost}&Status={address.Status}&DateCreated={DateTime.Now.ToString("yyyy-MM-ddTHH:mm:ss")}&CumstomerID={address.CumstomerID}";
+            var response = await httpClient.PutAsync(apiUrl, null);
+            return RedirectToAction("GetAllAddress");
+        }
+        public async Task<IActionResult> DeleteAddress(Guid id)
+        {
+            var address = _repos.GetAll().FirstOrDefault(c => c.AddressID == id);
+            HttpClient httpClient = new HttpClient();
+            string apiUrl = $"https://localhost:7036/api/Address/delete-address?id={id}";
+            var response = await httpClient.DeleteAsync(apiUrl);
+            return RedirectToAction("GetAllAddress");
+        }
+    }
+}
---
+++ AppView/Controllers/ColorsController.cs
@@ -0,0 +1,97 @@
+﻿using AppData.IRepositories;
+using AppData.Models;
+using AppData.Repositories;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Newtonsoft.Json;
+
+
+namespace AppView.Controllers
+{
+    public class ColorsController : Controller
+    {
+        private readonly IAllRepositories<Color> _repos;
+        private ShopDBContext _dbContext = new ShopDBContext();
+        private DbSet<Color> _color;
+        public ColorsController()
+        {
+            _color = _dbContext.Colors;
+            AllRepositories<Color> all = new AllRepositories<Color>(_dbContext, _color);
+            _repos = all;
+        }
+        // GET: ColorsController
+        //public ActionResult Index()
+        //{
+        //    return View();
+        //}
+
+        // GET: ColorsController/Details/5
+        private string GenerateColorCode()
+        {
+            var lastColor = _dbContext.Colors.OrderByDescending(c => c.ColorCode).FirstOrDefault();
+            if (lastColor != null)
+            {
+                var lastNumber = int.Parse(lastColor.ColorCode.Substring(2)); // Lấy phần số cuối cùng từ ColorCode
+                var nextNumber = lastNumber + 1; // Tăng giá trị cuối cùng
+                var newColorCode = "CL" + nextNumber.ToString("D3"); // Tạo ColorCode mới
+                return newColorCode;
+            }
+            return "CL001"; // Trường hợp không có ColorCode trong cơ sở dữ liệu, trả về giá trị mặc định "CL001"
+        }
+        public async Task<IActionResult> GetAllColor()
+        {
+
+            string apiUrl = "https://localhost:7036/api/Color/get-color";
+            var httpClient = new HttpClient();
+            var response = await httpClient.GetAsync(apiUrl);
+            string apiData = await response.Content.ReadAsStringAsync();
+            // Lấy kqua trả về từ API
+            // Đọc từ string Json vừa thu được sang List<T>
+            var color = JsonConvert.DeserializeObject<List<Color>>(apiData);
+            return View(color);
+        }
+        [HttpGet]
+        public async Task<IActionResult> CreateColor()
+        {
+            return View();
+        }
+        [HttpPost]
+        public async Task<IActionResult> CreateColor(Color color)
+        {
+            var httpClient = new HttpClient();
+            string apiUrl = $"https://localhost:7036/api/Color/create-color?ColorCode={GenerateColorCode()}&Name={color.Name}&Status={color.Status}&DateCreated={color.DateCreated.ToString("yyyy-MM-ddTHH:mm:ss")}";
+            var response = await httpClient.PostAsync(apiUrl, null);
+            return RedirectToAction("GetAllColor");
+        }
+
+
+        [HttpGet]
+        public async Task<IActionResult> EditColor(Guid id) // Khi ấn vào Create thì hiển thị View
+        {
+            // Lấy Product từ database dựa theo id truyền vào từ route
+            Color color = _repos.GetAll().FirstOrDefault(c => c.ColorID == id);
+            return View(color);
+        }
+        public async Task<IActionResult> EditColor(Color color) // Thực hiện việc Tạo mới
+        {
+            var httpClient = new HttpClient();
+            string apiUrl = $"https://localhost:7036/api/Color/update-color?ColorCode={color.ColorCode}&Name={color.Name}&Status={color.Status}&DateCreated={color.DateCreated}&ColorID={color.ColorID.ToString("yyyy-MM-ddTHH:mm:ss")}";
+            var response = await httpClient.PutAsync(apiUrl, null);
+            return RedirectToAction("GetAllColor");
+        }
+        public async Task<IActionResult> DeleteColor(Guid id)
+        {
+            var cus = _repos.GetAll().First(c => c.ColorID == id);
+            var httpClient = new HttpClient();
+            string apiUrl = $"https://localhost:7036/api/Color/delete-color?id={id}";
+            var response = await httpClient.DeleteAsync(apiUrl);
+            return RedirectToAction("GetAllColor");
+        }
+        public async Task<IActionResult> FindColor(string searchQuery)
+        {
+            var color = _repos.GetAll().Where(c => c.ColorCode.ToLower().Contains(searchQuery.ToLower()) || c.Name.ToLower().Contains(searchQuery.ToLower()));
+            return View(color);
+        }
+    }
+}
+
